c792efbeb1470c50a56902e12693c47cfc4e0a9a
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-03-03 13:41:37 -0500

    working on depthFirst

--- problems/graph.js
+++ problems/graph.js
@@ -18,10 +18,26 @@ class Graph {
 
   buildGraph(edges) {
     // Code goes here ...
+    // let graph = {};
+    edges.forEach(nodePair => {
+      this.addEdges(nodePair[0], nodePair[1]);
+    })
+    return this.adjList;
   }
 
   breadthFirstTraversal(startingVertex) {
     // Code goes here ...
+    let visited = new Set();
+    let q = [startingVertex];
+    let returnArr = [];
+    while(q.length) {
+      let currentEl = q.shift();
+      if(visited.has(currentEl)) continue;
+      visited.add(currentEl);
+      returnArr.push(currentEl);
+      q.push(...this.adjList[currentEl]);
+    }
+    return returnArr;
   }
 
   depthFirstTraversalIterative(startingVertex) {
