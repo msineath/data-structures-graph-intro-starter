ee328da02f9fd917b6c0f3297cbf436b8f9ef671
nathanieldcooke <nathanield.cooke@gmail.com>
2021-03-03 10:17:05 -0800

    solves specs 1-5

--- problems/graph.js
+++ problems/graph.js
@@ -1,15 +1,19 @@
 
 class Graph {
   constructor() {
-    // Code goes here ...
+    this.adjList = {};
   }
 
   addVertex(vertex) {
-    // Code goes here ...
+    if (!this.adjList[vertex]) this.adjList[vertex] = [];
   }
 
   addEdges(srcValue, destValue) {
-    // Code goes here ...
+    if (!this.adjList[srcValue]) this.addVertex(srcValue);
+    if (!this.adjList[destValue]) this.addVertex(destValue);
+
+    this.adjList[srcValue].push(destValue);
+    this.adjList[destValue].push(srcValue);
   }
 
   buildGraph(edges) {
